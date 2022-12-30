e77de8627c37948d39ce034b49c12ee8426dc64e
Adi Purnama <imadeadipurnamayasa@gmail.com>
2022-12-30 16:41:55 +0800

    update receive

--- application/views/member/topup/receive.php
+++ application/views/member/topup/receive.php
@@ -19,12 +19,11 @@
                                 ($_SESSION["currency"] == "GBP") ||
                                 ($_SESSION["currency"] == "RON")
                             ) { ?>
-                            <button type="button" class="receive-attention d-flex flex-column text-center col-12 p-3"
-                                data-bs-toggle="modal" data-bs-target="#attention">
-                                <span class="mb-2 fw-bold title">ATTENTION!</span>
-                                <span class="mb-2 middle">Before make any topup</span>
-                                <span class="bottom">read carefully how does it work</span>
-                            </button>
+                                <button type="button" class="receive-attention d-flex flex-column text-center col-12 p-3" data-bs-toggle="modal" data-bs-target="#attention">
+                                    <span class="w-100 mb-2 fw-bold title">ATTENTION!</span>
+                                    <span class="w-100 mb-2 middle">Before make any topup</span>
+                                    <span class="w-100 bottom">read carefully how does it work</span>
+                                </button>
                             <?php } ?>
                         </div>
                         <div class="col-12 recive-bank  d-flex align-items-center flex-column text-center">
@@ -41,31 +40,31 @@
                                 ($_SESSION["currency"] == "GBP") ||
                                 ($_SESSION["currency"] == "RON")
                             ) { ?>
-                            <a href="<?= base_url() ?>receive/localbank" class="col-8 py-3 my-2">Local bank</a>
+                                <a href="<?= base_url() ?>receive/localbank" class="col-8 py-3 my-2">Local bank</a>
                             <?php } else { ?>
-                            <div class="receive-note">
-                                <span class="fw-bold title">To top up this currency you have to covert from another
-                                    currency
-                                    in <b>SWAP</b>
-                                    section</span><br>
-                                <hr>
-                                <span>Or make an international bank transfer toward EURO or
-                                    DOLLAR</span>
-                                <hr>
-                            </div>
+                                <div class="receive-note">
+                                    <span class="fw-bold title">To top up this currency you have to covert from another
+                                        currency
+                                        in <b>SWAP</b>
+                                        section</span><br>
+                                    <hr>
+                                    <span>Or make an international bank transfer toward EURO or
+                                        DOLLAR</span>
+                                    <hr>
+                                </div>
 
-                            <a href="<?= base_url() ?>receive/interbank?currency=USD" class="col-8 py-3 my-2">USD
-                                International</a>
-                            <a href="<?= base_url() ?>receive/interbank?currency=EUR" class="col-8 py-3 my-2">EUR
-                                International</a>
-                            <div class="receive-note">
-                                <span>
-                                    <b>ATTENTION:<br>
-                                        UPON THE ARRIVAL OF THE BANK TRANSFER, THE SENT CURRENCY WILL BE CONVERTED INTO
-                                        THE
-                                        CHOSEN DESTINATION CURRENCY</b>
-                                </span>
-                            </div>
+                                <a href="<?= base_url() ?>receive/interbank?currency=USD" class="col-8 py-3 my-2">USD
+                                    International</a>
+                                <a href="<?= base_url() ?>receive/interbank?currency=EUR" class="col-8 py-3 my-2">EUR
+                                    International</a>
+                                <div class="receive-note">
+                                    <span>
+                                        <b>ATTENTION:<br>
+                                            UPON THE ARRIVAL OF THE BANK TRANSFER, THE SENT CURRENCY WILL BE CONVERTED INTO
+                                            THE
+                                            CHOSEN DESTINATION CURRENCY</b>
+                                    </span>
+                                </div>
 
                             <?php } ?>
                             <?php
@@ -73,7 +72,7 @@
                                 ($_SESSION["currency"] == "EUR") ||
                                 ($_SESSION["currency"] == "GBP")
                             ) { ?>
-                            <a href="<?= base_url() ?>receive/interbank" class="col-8 py-3 my-2">International</a>
+                                <a href="<?= base_url() ?>receive/interbank" class="col-8 py-3 my-2">International</a>
 
                             <?php } ?>
                         </div>
@@ -94,25 +93,23 @@
             </div>
             <div class="modal-body">
                 <ul>
-                    <li>In order to topup your own wallet or receive money through bank transfer, <br>
-                        copy or share the bank details written in ''add/receive funds'' by bank transfer section (local
-                        bank, international or cash) and as a causal IS MANDATORY TO WRITE :<br>
+                    <li>To top up your wallet or receive money by bank transfer,
+                        copy or share the bank details written in the section "add/receive funds" Local or international bank; the causal for payment IS MANDATORY AND IT IS MANDATORY TO WRITE:
+                        <br>
                         <br>
                         TOP UP + YOUR UNIQUE CODE NUMBER<br>
-                        (THE UNIQUE CODE IDENTIFY THE RECEIVER USER)<br>
+                        (THE UNIQUE CODE IDENTIFIES THE RECEIVER USER)<br>
                         <br>
-                        EXAMPLE: ''causal : topup xxxxxxxx''<br>
+                        EXAMPLE: "causal : topup xxxxxxxx"<br>
                         <img src="<?= base_url() ?>assets/img/attantion-1.png" alt="">
                     </li>
 
-                    <li>The receiving banking data of each currency will be the same for all users, THE ONLY DATA THAT
-                        WILL CHANGE WILL BE THE "CAUSAL" WHICH WILL IDENTIFY THE RECEIVER USER, SO IF YOU WANT TO TOP UP
-                        YOUR FRIEND WALLET YOU HAVE TO WRITE IN THE CAUSAL:<br>
+                    <li>receiving bank data for each currency will be the same for all users, THE ONLY DATUM THAT WILL VARY WILL BE THE "CAUSAL" THAT IDENTIFIES THE RECEIVING USER, SO IF YOU WANT TO TOP UP YOUR <b translate="no">PIGGYWALLET</b> YOU MUST WRITE IN THE CAUSAL:<br>
                         <br>
-                        topup + THE UNIQUE CODE OF HIS OWN WALLET
+                        TOP UP + UNIQUE CODE OF YOUR WALLET
                     </li>
-                    <li>To increase and improve the privacy level, receiving and sending money
-                        will be handled by TracklessMoney payment gateway</li>
+                    <li>To increase and improve the level of privacy, receiving and sending money will be handled by the payment gateway.</li>
+                    <li>IN THE ABSENCE OF THE CAUSAL OR OF THE CORRECT COMPILATION OF ALL THE DATA, THE MONEY WILL BE LOST AND NO LONGER RECOVERABLE.</li>
                 </ul>
             </div>
         </div>
